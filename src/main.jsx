334e1f47b79c799a9c586f156a691e31017ce92c
zacwong2151 <111118549+zacwong2151@users.noreply.github.com>
2023-06-19 16:11:31 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Context from './UserContext.jsx'
+import Context from './contexts/UserContext.jsx'
 
 import {
   createBrowserRouter,
