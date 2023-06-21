f36ea7e969e236ca4be20ed2d79cc496906081d1
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-06-22 02:18:38 +0800

    Resolve merge errors

--- src/pages/LostFoundItemForm.jsx
+++ src/pages/LostFoundItemForm.jsx
@@ -4,27 +4,32 @@ import ItemView from "../components/ItemView";
 import NavigationBar from "../components/NavigationBar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import FilterBar from "../components/FilterBar";
-import { Link } from "react-router-dom"
+import ItemTabs from "../components/ItemTabs";
+import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
-import { collection, doc, updateDoc, getDocs, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  updateDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { auth, db } from "../firebase";
 
 export default function LostFoundItemForm() {
   const { user } = useContext(UserContext);
 
-  const [returned, setReturned] = useState(false)
+  const [returned, setReturned] = useState(false);
   const [itemName, setItemName] = useState("");
   const [date, setDate] = useState("");
   const [colour, setColour] = useState("");
-  const [location, setLocation] = useState("")
-  const [picture, setPicture] = useState('')
-
-
+  const [location, setLocation] = useState("");
+  const [picture, setPicture] = useState("");
 
   const [error, setError] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   /*
     separate the lost and found form
@@ -35,7 +40,9 @@ export default function LostFoundItemForm() {
       return setError("Please fill in the item name");
     }
     if (date === "") {
-      return setError("Please fill in the approximate date and time you lost your item");
+      return setError(
+        "Please fill in the approximate date and time you lost your item"
+      );
     }
     if (colour === "") {
       return setError("Please fill in the colour of your lost item");
@@ -43,22 +50,25 @@ export default function LostFoundItemForm() {
     if (location === "") {
       return setError("Please fill in the locaion you lost your item");
     }
-    console.log('0')
+    console.log("0");
     try {
       setError("");
       setLoading(true);
-      console.log('1')
+      console.log("1");
 
       // Retrieve the user's document from Firestore
-      const email = `${user.email}`
+      const email = `${user.email}`;
       const usersCollectionRef = collection(db, "users");
-      const queryByEmail = query(usersCollectionRef, where("email", "==", email));
+      const queryByEmail = query(
+        usersCollectionRef,
+        where("email", "==", email)
+      );
       const querySnapshot = await getDocs(queryByEmail);
 
       if (!querySnapshot.empty) {
         // Retrieve the current "lostItems" array from the user's document
         const currentLostItems = querySnapshot.docs[0].data().lostItems;
-        console.log(currentLostItems)
+        console.log(currentLostItems);
 
         // Create a new object with the lost item data
         const newLostItem = {
@@ -67,47 +77,44 @@ export default function LostFoundItemForm() {
           dateAndTime,
           itemDescription,
           picture,
-        }
+        };
 
         // Update the "lostItems" array by pushing the new lost item
         const updatedLostItems = [...currentLostItems, newLostItem];
-        console.log(updatedLostItems)
+        console.log(updatedLostItems);
 
         // Update the user's document with the updated "lostItems" array
         const userDocRef = querySnapshot.docs[0].ref;
         await updateDoc(userDocRef, { lostItems: updatedLostItems });
-        console.log('4')
+        console.log("4");
 
         // resetInputFields();
         // setSuccessMessage("Sign up successful!");
         // console.log("User signed up successfully.");
       } else {
-        console.log("User document does not exist")
+        console.log("User document does not exist");
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
     setLoading(false);
-  }
-
-
-
+  };
 
   function handleSubmit(e) {
-    e.preventDefault()
-    handleForm()
+    e.preventDefault();
+    handleForm();
   }
 
   return (
     <>
-      {user ? `${user.contact}, ${user.email}, ${user.password}` : 'no user'}
+      {user ? `${user.contact}, ${user.email}, ${user.password}` : "no user"}
       <Row className="justify-content-md-center">
         <Col lg="8">
           <div>
             <NavigationBar />
           </div>
           <hr />
-          <FilterBar />
+          <ItemTabs />
           <br />
           <div>
             <form onSubmit={handleSubmit} className="form">
@@ -173,7 +180,6 @@ export default function LostFoundItemForm() {
               <button disabled={loading} className="log-in-sign-up-button">
                 Submit form
               </button>
-
             </form>
           </div>
         </Col>
@@ -182,6 +188,5 @@ export default function LostFoundItemForm() {
         <Link to="/home-page">Back</Link>
       </button>
     </>
-
-  )
+  );
 }
