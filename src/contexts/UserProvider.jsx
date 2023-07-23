006598e5e0d61d48c53eb9864407aff94b84ff61
Andre Chua <110965035+andrechuakj@users.noreply.github.com>
2023-07-23 16:27:07 +0800

    attempt at fixing navigate issues upon first visiting site

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -10,23 +10,33 @@ import UserContext from "./UserContext";
 const UserProvider = ({ children }) => {
   const initialUser = JSON.parse(localStorage.getItem("user")) || null;
   const [user, setUser] = useState(initialUser);
+  const [isCheckingUser, setIsCheckingUser] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser !== "null") {
-      if (
-        ["/login", "/signup", "/forgot-password"].includes(
-          window.location.pathname
-        )
-      ) {
-        navigate("/");
-      }
       setUser(JSON.parse(storedUser));
-    } else {
-      navigate("/login");
     }
+    setIsCheckingUser(false);
   }, []);
+
+  useEffect(() => {
+    if (!isCheckingUser) {
+      // Navigate based on the user's login status
+      if (user) {
+        // User is logged in, navigate to home or other authenticated pages
+        if (["/login", "/signup", "/forgot-password"].includes(window.location.pathname)) {
+          navigate("/");
+        }
+      } else {
+        // User is not logged in, navigate to login page
+        if (!["/login", "/signup", "/forgot-password"].includes(window.location.pathname)) {
+          navigate("/login");
+        }
+      }
+    }
+  }, [user, isCheckingUser]);
 
   useEffect(() => {
     localStorage.setItem("user", JSON.stringify(user));
